91a7432ec05e116804ddf58cb6db149d16d0a35f
BrindhaSelvaraj <itzmibrindha@gmail.com>
2024-03-06 19:29:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskManager from './assets/Components/TaskManager';
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+      <TaskManager />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/Components/TaskManager.jsx
@@ -0,0 +1,73 @@
+// TaskManager.jsx
+import React, { useState } from 'react';
+import AddTaskForm from './AddTaskForm';
+import TaskList from './TaskList';
+
+const TaskManager = () => {
+  const [tasks, setTasks] = useState([]);
+  const [statusFilter, setStatusFilter] = useState('all');
+
+  const handleAddTask = (newTask) => {
+    const updatedTasks = [...tasks, { ...newTask, id: Date.now() }];
+    setTasks(updatedTasks);
+  };
+
+  const handleDeleteTask = (taskId) => {
+    setTasks(tasks.filter(task => task.id !== taskId));
+  };
+
+  const handleEditTask = (taskId, newTitle, newDescription) => {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          title: newTitle,
+          description: newDescription
+        };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
+  const handleStatusChangeTask = (taskId, newStatus) => {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          status: newStatus
+        };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
+  const handleStatusFilterChange = (e) => {
+    setStatusFilter(e.target.value);
+  };
+
+  const filteredTasks = statusFilter === 'all' ? tasks : tasks.filter(task => task.status === statusFilter);
+
+  return (
+    <div>
+      <div style={{ marginBottom: '20px', display: 'flex', justifyContent: 'space-between' }}>
+        <div>
+          <AddTaskForm onAddTask={handleAddTask} />
+        </div>
+      </div>
+      <div style={{ textAlign: 'right' }}>
+        <select value={statusFilter} onChange={handleStatusFilterChange}>
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="pending">Pending</option>
+        </select>
+      </div>
+      <div>
+        <TaskList tasks={filteredTasks} onDeleteTask={handleDeleteTask} onEditTask={handleEditTask} onChangeStatusTask={handleStatusChangeTask} />
+      </div>
+    </div>
+  );
+};
+
+export default TaskManager;
